b8c198561e44c9ea124e337822b8c82965beac9b
jffryi <jefferyi.magtubo@gmail.com>
2024-03-20 20:33:37 -0500

    Redid project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: [],
   theme: {
-    extend: {
-      colors: {
-        'darkBlack': "#14171C",
-        'lightBlack': "#2C343F",
-        'text': "#556678",
-        'lttrbxdGreen': "#00B021",
-        'lttrbxdOrg': "#F27405"
-      },
-      fontFamily: {
-        'karla': ["Karla", 'sans-serif']
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
